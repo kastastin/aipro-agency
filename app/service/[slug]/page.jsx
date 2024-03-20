fb667d3acb0c323a89ccbbd962646ffa26eaccf8
kastastin <kostya242421@gmail.com>
2024-03-20 12:45:44 +0200

    services pages (design data | 375px)

--- app/service/[slug]/page.jsx
+++ app/service/[slug]/page.jsx
@@ -1,6 +1,10 @@
+"use client";
+
 import { notFound } from "next/navigation";
+import Image from "next/image";
 
 import { services } from "@/app/service/data";
+import DiscussionForm from "@/components/DiscussionForm";
 
 export default function ServicePage({ params }) {
   const service = services.find(
@@ -10,10 +14,60 @@ export default function ServicePage({ params }) {
   if (!service) return notFound();
 
   return (
-    <div>
-      <h1>ServicePage</h1>
+    <section className="py-[85px]">
+      <div className="container mx-auto">
+        <h1 className="h2 mb-[20px]">{service.name}</h1>
+        <p className="text-sm">{service.description}</p>
+        {/* Types */}
+        <ul className="my-[50px] space-y-[20px]">
+          {service.types.map((type, index) => (
+            <li key={index}>
+              <span className="text-xl">{type}</span>
+            </li>
+          ))}
+        </ul>
+        {/* Section image */}
+        <div className="relative mb-[40px]">
+          <div className="relative h-[195px] w-[345px] overflow-hidden">
+            <Image
+              src={service.imageSrc}
+              alt={service.name}
+              fill
+              className="scale-[1.1] object-cover"
+            />
+          </div>
 
-      <p>{service.name}</p>
-    </div>
+          <Image
+            src="/services/stone-top.png"
+            alt="Stone"
+            width={80}
+            height={130}
+            className="absolute -right-[20px] -top-[35px] z-10 rotate-[40deg]"
+          />
+
+          <Image
+            src="/services/stone-bottom.png"
+            alt="Stone"
+            width={60}
+            height={110}
+            className="absolute -bottom-[25px] left-[5px] z-10 "
+          />
+        </div>
+
+        <p className="text-sm">{service.conclusion}</p>
+
+        <h2 className="h2 mb-[50px] mt-[100px]">
+          Давайте <span className="text-primary">обсудим</span> задачу
+        </h2>
+
+        <p className="mb-[50px] text-sm">
+          Заполните контактную форму, чтобы раскрыть свои потребности и цели,
+          получив бесплатную консультацию с 10% скидкой на все услуги AiPro
+          Agency.
+        </p>
+
+        <DiscussionForm />
+      </div>
+    </section>
   );
 }
